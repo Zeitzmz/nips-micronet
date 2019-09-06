f9ed25dbbfa1ac829767f0de4a3ebba4da83b457
zengmengze <zengmengze@momenta.ai>
2019-09-06 10:19:56 +0800

    change gpu id

--- submit-model/test.sh
+++ submit-model/test.sh
@@ -8,5 +8,5 @@ mkdir -p log
 ${caffe_tools}/caffe test \
 --model=caffe_model/fp16.prototxt \
 --weights=caffe_model/fp16.caffemodel \
---iterations 2000 \
---gpu=1 2>&1 | tee ./log/test.log 
+--iterations=2000 \
+--gpu=0 2>&1 | tee ./log/test.log 
